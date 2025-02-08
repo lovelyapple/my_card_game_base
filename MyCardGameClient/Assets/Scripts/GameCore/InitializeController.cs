34c1374bcfaa4d9c61a20caed003e1a80d07e4a8
li lin <lovelyappleoffie@gmial.com>
2025-02-08 19:08:20 +0900

    簡単なシーンの遷移ができるまで*

--- MyCardGameClient/Assets/Scripts/GameCore/InitializeController.cs
+++ MyCardGameClient/Assets/Scripts/GameCore/InitializeController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class InitializeController : MonoSingletonBase<InitializeController>
 {
+    private enum State
+    {
+        None,
+        RequestGo,
+        Going,
+    }
+    private State _state = State.None;
     private List<GameModel> _gameModels = new List<GameModel>
     {
         new GameModel(),
     };
     public override void OnAwake()
     {
-        foreach(var model in _gameModels)
+        foreach (var model in _gameModels)
         {
             model.SetInsance(model);
+        }
+
+        var sceneTransit = new SceneTransit();
+        sceneTransit.SetInsance(sceneTransit);
+
+        _state = State.RequestGo;
+    }
+    public void Update()
+    {
+        if (_state == State.RequestGo)
+        {
+            SceneTransit.GotoScene("Title");
+            _state = State.Going;
         }
     }
 }
--- MyCardGameClient/Assets/Scripts/GameCore/SceneTransit.cs
+++ MyCardGameClient/Assets/Scripts/GameCore/SceneTransit.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneTransit : SingletonBase<SceneTransit>
 {
     private string _currentScene = null;
-    public void RequestGotoScene(string sceneName)
+    private void RequestGotoScene(string sceneName)
     {
-        if(string.IsNullOrEmpty(_currentScene))
+        if (string.IsNullOrEmpty(_currentScene))
         {
             _currentScene = SceneManager.GetActiveScene().name;
         }
 
-        
+        GameMainObject.Instance.StartCoroutine(RequestChangeSceneAsync(sceneName));
+    }
+    private IEnumerator RequestChangeSceneAsync(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        _currentScene = sceneName;
+    }
+    public static void GotoScene(string sceneName)
+    {
+        GetInstance().RequestGotoScene(sceneName);
     }
 }
